f209cfed8f9ccb55f466309ba599d3da6ded0e37
Guillermo Mercado <guillermo.mercado26@gmail.com>
2025-03-01 23:21:26 -0300

    Implement form validation and submission handling in script.js

--- js/script.js
+++ js/script.js
@@ -56,16 +56,14 @@ console.log(nuevoEnlace);
 
 // Seleccionar elementos y asociarles un evento
 
-const btnEnviar = document.querySelector('.boton--primario');
-btnEnviar.addEventListener('click', function(evento) {
-    console.log(evento);
-    evento.preventDefault(); // previene la acción por defecto
-    // Validar un formulario
+// const btnEnviar = document.querySelector('.boton--primario');
+// btnEnviar.addEventListener('click', function(evento) {
+//     console.log(evento);
+//     evento.preventDefault(); // previene la acción por defecto
+//     // Validar un formulario
 
-
-
-    console.log('Enviando formulario');
-});
+//     console.log('Enviando formulario');
+// });
 
 // Eventos de los Inputs y Textarea
 
@@ -78,18 +76,61 @@ const datos = {
 const nombre = document.querySelector('#nombre');
 const email = document.querySelector('#email');
 const mensaje = document.querySelector('#mensaje');
-
+const formulario = document.querySelector('.formulario');
 
 nombre.addEventListener('input', leerTexto);
 email.addEventListener('input', leerTexto);
 mensaje.addEventListener('input', leerTexto);
+
+// El evento de submit
+
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+    //Validar el formulario
+    const {nombre, email, mensaje} = datos;
+
+    if(nombre === '' || email === '' || mensaje === '') {
+        monstrarError("Todos los campos son obligatorios");
+        return;
+    }else {
+        monstrarEnvio("Mensaje enviado correctamente");
+    }
+
+    //Enviar el formulario
+    console.log('Se envio el formulario');
+});
 
 function leerTexto(e){
     console.log(e.target.value);
 
     datos[e.target.id] = e.target.value;
 
-    console.log(datos);
+    // console.log(datos);
 }
 
+// Mostrar Error
+function monstrarError(mensaje) {
+    const error= document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error');
+    formulario.appendChild(error);
+
+    //Eliminar el mensaje de error después de 5 segundos
+    setTimeout(() => {
+        error.remove();
+    }, 5000);
+}
+
+// Mostrar envio correcto
+function monstrarEnvio(mensaje) {
+    const success= document.createElement('P');
+    success.textContent = mensaje;
+    success.classList.add('correcto');
+    formulario.appendChild(success);
+
+    //Eliminar el mensaje de error después de 5 segundos
+    setTimeout(() => {
+        success.remove();
+    }, 5000);
+}
 
