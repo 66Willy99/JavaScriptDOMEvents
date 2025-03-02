7fbbabcbb97203bcdb18d84ce08aa382d851c3ab
Guillermo Mercado <guillermo.mercado26@gmail.com>
2025-03-01 22:06:25 -0300

    Add script.js for header manipulation and include in index.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// QuerySelector
+
+const heading = document.querySelector('.header__texto h2') // 0 o 1 elemento
+// heading.textContent = 'Nuevo Heading';
+console.log(heading);
+
+// QuerySelectorAll
+
+
+
+// getElementById
