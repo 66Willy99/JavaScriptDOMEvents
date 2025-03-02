fc20ec997959db1a9d3da52d8249c2ac98cac8ad
Guillermo Mercado <guillermo.mercado26@gmail.com>
2025-03-01 22:22:47 -0300

    Add functionality to create and append a new navigation link in script.js

--- js/script.js
+++ js/script.js
@@ -9,9 +9,26 @@ console.log(heading);
 const enlaces = document.querySelectorAll('.navegacion a');
 enlaces[0].textContent = 'Nuevo Texto para Enlace';
 enlaces[0].classList.add('nueva-clase');
-enlaces[0].classList.remove('navegacion__enlace');
 
 // getElementById
 // esta sintaxis ya no se ocupa tanto como querySelector o querySelectorAll
 const heading2 = document.getElementById('heading'); 
 console.log(heading2);
+
+
+// Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A'); // JavaScript recomienda crearlo con mayusculas y sin apertura y cierra
+// Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+// Agregar el texto
+nuevoEnlace.textContent = 'Tienda Virtual';
+
+//Agregar la clace
+nuevoEnlace.classList.add('navegacion__enlace');
+
+// Agregarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
