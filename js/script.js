060b72ba42ee67878b20aed8a2caeb3e2bbe691f
Guillermo Mercado <guillermo.mercado26@gmail.com>
2025-03-01 22:14:25 -0300

    Add ID to heading and update link text and classes in script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ console.log(heading);
 
 // QuerySelectorAll
 
-
+const enlaces = document.querySelectorAll('.navegacion a');
+enlaces[0].textContent = 'Nuevo Texto para Enlace';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
 
 // getElementById
+// esta sintaxis ya no se ocupa tanto como querySelector o querySelectorAll
+const heading2 = document.getElementById('heading'); 
+console.log(heading2);
